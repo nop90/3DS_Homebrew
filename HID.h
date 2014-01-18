2f776741d046fd8f8c55160c9a972b61b7f58074
nop90 <nop90nop90@gmail.com>
2014-01-18 01:13:36 +0100

    Create HID.h

---
+++ HID.h
@@ -0,0 +1,17 @@
+ifndef HID_H
+#define MEMORY_H
+
+#define BUTTON_A 1
+#define BUTTON_B 2
+#define BUTTON_UP 64
+#define BUTTON_LEFT 32
+#define BUTTON_DOWN 128
+#define BUTTON_RIGHT 16
+#define BUTTON_X 1024
+#define BUTTON_Y 2048
+#define BUTTON_L1 512
+#define BUTTON_R1 256
+#define BUTTON_START 8
+#define BUTTON_SELECT 4
+
+#endif
